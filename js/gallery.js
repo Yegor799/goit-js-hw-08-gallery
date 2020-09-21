6eaf8f51ed294d5389d9553a4e0a080a4b75400a
Yegor Yefimov <egoryefim@gmail.com>
2020-09-22 01:24:56 +0300

    Добавил разметку галереи

---
+++ js/gallery.js
@@ -0,0 +1,28 @@
+import galleryItems from './gallery-items.js';
+
+const galleryContiner = document.querySelector('.js-gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContiner.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `;
+    })
+    .join('');
+}
