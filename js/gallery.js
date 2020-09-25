e66c096b55d5534c5fbd2e2ce968a1172c22f820
Yegor Yefimov <egoryefim@gmail.com>
2020-09-25 09:26:13 +0300

    Добавил закрытие модального окна при нажатии esc и при нажатии на overlay

--- js/gallery.js
+++ js/gallery.js
@@ -4,10 +4,13 @@ const galleryContiner = document.querySelector('.js-gallery');
 const lightboxEl = document.querySelector('.js-lightbox');
 const lightBoxImageEl = document.querySelector('.lightbox__image');
 const closeModalBtn = document.querySelector('[data-action="close-lightbox"]');
+const lightboxContentEl = document.querySelector('.lightbox__content');
 
-closeModalBtn.addEventListener('click', onCloseBtnClick);
+closeModalBtn.addEventListener('click', closeModal);
 
 galleryContiner.addEventListener('click', onGalleryClick);
+
+lightboxContentEl.addEventListener('click', onOverlayClick);
 
 const galleryMarkup = createGalleryMarkup(galleryItems);
 
@@ -35,16 +38,33 @@ function createGalleryMarkup(galleryItems) {
 }
 
 function onGalleryClick(event) {
+  event.preventDefault();
   if (event.target.nodeName !== 'IMG') {
     return;
   }
-  event.preventDefault();
+  window.addEventListener('keydown', onEscKeyPress);
 
   lightboxEl.classList.add('is-open');
   lightBoxImageEl.src = event.target.dataset.source;
+  lightBoxImageEl.alt = event.target.alt;
 }
 
-function onCloseBtnClick() {
+function closeModal() {
+  window.removeEventListener('keydown', onEscKeyPress);
   lightboxEl.classList.remove('is-open');
   lightBoxImageEl.src = '';
+}
+
+function onOverlayClick(event) {
+  if (event.target === event.currentTarget) {
+    closeModal();
+  }
+}
+
+function onEscKeyPress(event) {
+  const isEscape = event.code === 'Escape';
+
+  if (isEscape) {
+    closeModal();
+  }
 }
